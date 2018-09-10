3e50a10ad038203f25dcf32f1c274172f9ac4b62
Elrey Belmonti <elrey.belmonti@formidable.com>
2018-09-10 16:41:51 -0700

    add objects to styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,91 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import style from 'styled-components';
+import style, { ThemeProvider } from 'styled-components';
 
-const Card = style.div`
+const RedialCard = style.div`
+  font-size: ${props => {
+    if (props.xl) return props.theme.fontSize.xl;
+    if (props.lg) return props.theme.fontSize.lg;
+    if (props.md) return props.theme.fontSize.md;
+    if (props.sm) return props.theme.fontSize.sm;
+    return props.theme.fontSize.nm;
+  }};
   width: 500px;
   height: 500px;
-  background: radial-gradient(ellipse farthest-corner at center, red 0, blue, green 100%);
+  background: ${props => {
+    // console.log(typeof props.radial);
+    if (typeof props.radial === 'string') return props.radial;
+    if (typeof props.radial === 'object') {
+      const shape = props.radial.shape ? `${props.radial.shape},` : '';
+      const size = props.radial.size ? `${props.radial.size},` : '';
+      const position = props.radial.position ? `${props.radial.position},` : '';
+      return `radial-gradient( ${position} ${props.radial.startColor}, ${
+        props.radial.lastColor
+      })`;
+    }
+    if (props.radial) return 'radial-gradient(at top right, red, green, blue)';
+    return props.theme.colors.primary;
+  }};
 `;
+
+const theme = {
+  colors: {
+    primary: 'royalblue',
+    secondary: 'teal',
+    text: 'black'
+  },
+  fontSize: {
+    xl: '2.4rem',
+    lg: '1.8rem',
+    md: '1.3rem',
+    nm: '1rem',
+    sm: '0.75rem'
+  }
+};
+
+const diffRadial = 'radial-gradient(red, blue)';
+const objRadial = {
+  startColor: 'green',
+  lastColor: 'yellow'
+};
+const bigObjRadial = {
+  shape: 'circle',
+  size: 'farthest-corner',
+  position: 'at bottom left',
+  startColor: 'white',
+  lastColor: 'black'
+};
 
 class App extends Component {
   render() {
-    return <Card />;
+    // console.log(diffRadial);
+
+    return (
+      <div>
+        <ThemeProvider theme={theme}>
+          <RedialCard radial xl>
+            stuff
+          </RedialCard>
+        </ThemeProvider>
+        <ThemeProvider theme={theme}>
+          <RedialCard xl>stuff</RedialCard>
+        </ThemeProvider>
+        <ThemeProvider theme={theme}>
+          <RedialCard radial={diffRadial} xl>
+            stuff
+          </RedialCard>
+        </ThemeProvider>
+        <ThemeProvider theme={theme}>
+          <RedialCard radial={objRadial} xl>
+            stuff
+          </RedialCard>
+        </ThemeProvider>
+        <ThemeProvider theme={theme}>
+          <RedialCard radial={bigObjRadial} xl>
+            stuff
+          </RedialCard>
+        </ThemeProvider>
+      </div>
+    );
   }
 }
 
